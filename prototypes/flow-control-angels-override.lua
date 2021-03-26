2eacfd8a930ca2b3225ceada2e5defb6fbfb5edc
oldskoolmatt <74568293+oldskoolmatt@users.noreply.github.com>
2021-03-26 14:41:24 +0100

    Fixed wrong fucntion reference

--- prototypes/flow-control-angels-override.lua
+++ prototypes/flow-control-angels-override.lua
@@ -82,9 +82,11 @@ end
 -- Assign pictures to entities
 for material, map in pairs(material_map) do
 
+	-- Define local variables
 	local picture_path = "__reskins-angels__/graphics/entity/smelting/pipe/"
 	local icon_path = "/graphics/icon/reskin/angels"
-	local covers_path = "__reskins-angels__/graphics/entity/smelting/pipe-covers/"
+	local covers_path = flow_bob_covers_path
+	local shadow_covers_path = flow_bob_shadow_covers_path
 	
 	if data.raw["storage-tank"] and data.raw["storage-tank"]["pipe-" .. material .. "-straight"] then
 	
@@ -95,7 +97,7 @@ for material, map in pairs(material_map) do
 			south = flow_bob_str_pictures(material, picture_path).straight_vertical,
 			west = flow_bob_str_pictures(material, picture_path).straight_horizontal
 		}
-		data.raw["storage-tank"]["pipe-" .. material .. "-straight"].fluid_box.pipe_covers = flow_bob_pipe_covers_pictures(material, covers_path)
+		data.raw["storage-tank"]["pipe-" .. material .. "-straight"].fluid_box.pipe_covers = flow_bob_pipe_covers_pictures(material, covers_path, shadow_covers_path)
 	end
 	
 	if data.raw["storage-tank"] and data.raw["storage-tank"]["pipe-" .. material .. "-junction"] then
@@ -106,7 +108,7 @@ for material, map in pairs(material_map) do
 			south = flow_bob_jun_pictures(material, picture_path).t_up,
 			west = flow_bob_jun_pictures(material, picture_path).t_right
 		}
-		data.raw["storage-tank"]["pipe-" .. material .. "-junction"].fluid_box.pipe_covers = flow_bob_pipe_covers_pictures(material, covers_path)
+		data.raw["storage-tank"]["pipe-" .. material .. "-junction"].fluid_box.pipe_covers = flow_bob_pipe_covers_pictures(material, covers_path, shadow_covers_path)
 	end
 	
 	if data.raw["storage-tank"] and data.raw["storage-tank"]["pipe-" .. material .. "-elbow"] then
@@ -117,6 +119,6 @@ for material, map in pairs(material_map) do
 			south = flow_bob_elb_pictures(material, picture_path).corner_up_left,
 			west = flow_bob_elb_pictures(material, picture_path).corner_up_right
 		}
-		data.raw["storage-tank"]["pipe-" .. material .. "-elbow"].fluid_box.pipe_covers = flow_bob_pipe_covers_pictures(material, covers_path)
+		data.raw["storage-tank"]["pipe-" .. material .. "-elbow"].fluid_box.pipe_covers = flow_bob_pipe_covers_pictures(material, covers_path, shadow_covers_path)
 	end
 end
