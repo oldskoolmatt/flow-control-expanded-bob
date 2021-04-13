44602bd07a297414a7e31145d54ae9e60415f71c
Kira <35714562+kirazy@users.noreply.github.com>
2021-04-13 02:02:19 -0700

    Remove conditional require, update function requires

--- data-updates.lua
+++ data-updates.lua
@@ -1,25 +1,22 @@
 -- Generate pipes for boblogistics
 require("prototypes.flow-control-bob")
 
--- Overrides Angel's Smelting pipes
-if mods ["reskins-angels"] and mods ["angelssmelting"] then
--- if settings.startup[reskins-angels-use-angels-material-colors-pipes].value == true then
-	require("prototypes.flow-control-angels-override")
-end
+-- Conform pipe materials to those added by reskins-angels
+require("prototypes.flow-control-angels-override")
 
 -- Remove bob valves
-local remove_valve = flow_bob_remove_bob_valve
+local remove_valve = require("utils.lib").remove_bob_valve
 
 remove_valve("bob-valve")
 remove_valve("bob-overflow-valve")
 remove_valve("bob-topup-valve")
 
 -- Reskin valves
-local reskin_valve = flow_bob_reskin_valve
-
 if mods ["reskins-bobs"] then
+	local reskin_valve = require("utils.lib").reskin_valve
+
 	reskin_valve("check-valve")
 	reskin_valve("overflow-valve")
 	reskin_valve("underflow-valve")
-	reskin_valve("underflow-valve-2")	
+	reskin_valve("underflow-valve-2")
 end
