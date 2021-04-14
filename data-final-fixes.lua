2414a835df3da49124aa319c0aeaa1c48e912d84
oldskoolmatt <74568293+oldskoolmatt@users.noreply.github.com>
2021-04-14 03:55:13 +0200

    Update data-final-fixes.lua

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -30,7 +30,7 @@ local materials =
 }
 
 -- Generate subgroups for bob pipes
-make_subgroup("valves",			"z-v-01") -- valves
+make_subgroup("valves",		"z-v-01") -- valves
 make_subgroup("pipes-row-1",	"z-z-01") -- copper
 make_subgroup("pipes-row-2",	"z-z-02") -- iron
 make_subgroup("pipes-row-3",	"z-z-03") -- stone
@@ -46,13 +46,13 @@ make_subgroup("pipes-row-12",	"z-z-12") -- copper-tungsten
 
 -- Assign subgroup to valves
 assign_subgroup("bob-valve",			"valves") -- Flow control
-assign_subgroup("bob-overflow-valve",	"valves") -- Flow control
-assign_subgroup("bob-top-up-valve",		"valves") -- Flow control
-assign_subgroup("flowbob-top-up-valve",	"valves") -- Flow control
+assign_subgroup("bob-overflow-valve",		"valves") -- Flow control
+assign_subgroup("bob-topup-valve",		"valves") -- Flow control
+assign_subgroup("flowbob-topup-valve",		"valves") -- Flow control
 assign_subgroup("void-pump",			"valves") -- Flow control
 
 -- Assign subgroup to pipes
-assign_subgroup("pipe", 			"pipes-row-2", "1") -- Base
+assign_subgroup("pipe", 		"pipes-row-2", "1") -- Base
 assign_subgroup("pipe-to-ground", 	"pipes-row-2", "2") -- Base
 assign_subgroup("pipe-straight",	"pipes-row-2", "3") -- Flow control
 assign_subgroup("pipe-junction",	"pipes-row-2", "4") -- Flow control
@@ -62,7 +62,7 @@ for material, parameters in pairs(materials) do
 	local subgroup_row = "pipes-row-"..parameters.row
 
 	-- Assign subgroup
-	assign_subgroup(material.."-pipe",				subgroup_row, "1") -- pipe
+	assign_subgroup(material.."-pipe",		subgroup_row, "1") -- pipe
 	assign_subgroup(material.."-pipe-to-ground",	subgroup_row, "2") -- pipe to ground
 	assign_subgroup("pipe-"..material.."-straight", subgroup_row, "3") -- straight
 	assign_subgroup("pipe-"..material.."-junction", subgroup_row, "4") -- junction
@@ -80,24 +80,24 @@ make_subgroup("offshore-pumps",	"a-e-1") -- offshore pumps
 
 -- Storage tanks
 assign_subgroup("bob-small-inline-storage-tank",	"storage-tanks") -- Bobplates
-assign_subgroup("bob-small-storage-tank",			"storage-tanks") -- Bobplates
-assign_subgroup("storage-tank",						"storage-tanks") -- Boblogistics
-assign_subgroup("storage-tank-2",					"storage-tanks") -- Boblogistics
-assign_subgroup("storage-tank-3",					"storage-tanks") -- Boblogistics
-assign_subgroup("storage-tank-4",					"storage-tanks") -- Boblogistics
+assign_subgroup("bob-small-storage-tank",		"storage-tanks") -- Bobplates
+assign_subgroup("storage-tank",				"storage-tanks") -- Boblogistics
+assign_subgroup("storage-tank-2",			"storage-tanks") -- Boblogistics
+assign_subgroup("storage-tank-3",			"storage-tanks") -- Boblogistics
+assign_subgroup("storage-tank-4",			"storage-tanks") -- Boblogistics
 assign_subgroup("bob-storage-tank-all-corners",		"storage-tanks") -- Boblogistics
 assign_subgroup("bob-storage-tank-all-corners-2",	"storage-tanks") -- Boblogistics
 assign_subgroup("bob-storage-tank-all-corners-3",	"storage-tanks") -- Boblogistics
 assign_subgroup("bob-storage-tank-all-corners-4",	"storage-tanks") -- Boblogistics
 
 -- Pumps
-assign_subgroup("pump",			"pumps") -- Base
+assign_subgroup("pump",		"pumps") -- Base
 assign_subgroup("bob-pump-2",	"pumps") -- Boblogistics
 assign_subgroup("bob-pump-3",	"pumps") -- Boblogistics
 assign_subgroup("bob-pump-4",	"pumps") -- Boblogistics
 
 -- Compressors
-assign_subgroup("air-pump",		"compressors-1") -- Bobplates
+assign_subgroup("air-pump",	"compressors-1") -- Bobplates
 assign_subgroup("air-pump-2",	"compressors-1") -- Bobplates
 assign_subgroup("air-pump-3",	"compressors-1") -- Bobplates
 assign_subgroup("air-pump-4",	"compressors-1") -- Bobplates
@@ -107,7 +107,7 @@ assign_subgroup("water-pump-3",	"compressors-2") -- Bobplates
 assign_subgroup("water-pump-4",	"compressors-2") -- Bobplates
 
 -- Pumpjacks
-assign_subgroup("pumpjack",			"pumpjacks-1") -- Base
+assign_subgroup("pumpjack",		"pumpjacks-1") -- Base
 assign_subgroup("bob-pumpjack-1",	"pumpjacks-1") -- Bobmining
 assign_subgroup("bob-pumpjack-2",	"pumpjacks-1") -- Bobmining
 assign_subgroup("bob-pumpjack-3",	"pumpjacks-1") -- Bobmining
