f4c784e52964f8d9c6c4d1b7221cfb021c7b3737
oldskoolmatt <74568293+oldskoolmatt@users.noreply.github.com>
2021-08-23 22:04:04 +0200

    Delete data-updates.lua

--- data-updates.lua
+++
@@ -1,9 +0,0 @@
---------------------------
----- data-updates.lua ----
---------------------------
-
--- Generate pipes for boblogistics
-require("prototypes.flowbob-pipes")
-
--- Conform pipe materials to those added by reskins-angels
-require("prototypes.override.angels-overrides")
