124aa0f0768dae48a6f17646c938eac2d3baa337
oldskoolmatt <74568293+oldskoolmatt@users.noreply.github.com>
2021-08-23 21:29:20 +0200

    Delete prototypes/override directory

--- prototypes/override/final-overrides.lua
+++
@@ -1,9 +0,0 @@
-------------------------------
----- data-final-fixes.lua ----
-------------------------------
-
--- Fetch functions from library
-local add_tech = require("utils.lib").add_tech
-local rem_tech = require("utils.lib").rem_tech
-local rem_hide = require("utils.lib").remove_hidden_flag
-local remove_entity = require("utils.lib").remove_entity
